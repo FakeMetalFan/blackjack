86b5f6837b70dab140f8de4c27a6fea523123422
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-02 15:34:49 +0300

    Feature: added images preloading;

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,73 @@ import { Blackjack } from './Blackjack';
 
 import '@styles/index.scss';
 
+import '@img/card-back.png';
+
+import '@img/ace-of-clubs.png';
+import '@img/ace-of-diamonds.png';
+import '@img/ace-of-hearts.png';
+import '@img/ace-of-spades.png';
+
+import '@img/two-of-clubs.png';
+import '@img/two-of-diamonds.png';
+import '@img/two-of-hearts.png';
+import '@img/two-of-spades.png';
+
+import '@img/three-of-clubs.png';
+import '@img/three-of-diamonds.png';
+import '@img/three-of-hearts.png';
+import '@img/three-of-spades.png';
+
+import '@img/four-of-clubs.png';
+import '@img/four-of-diamonds.png';
+import '@img/four-of-hearts.png';
+import '@img/four-of-spades.png';
+
+import '@img/five-of-clubs.png';
+import '@img/five-of-diamonds.png';
+import '@img/five-of-hearts.png';
+import '@img/five-of-spades.png';
+
+import '@img/six-of-clubs.png';
+import '@img/six-of-diamonds.png';
+import '@img/six-of-hearts.png';
+import '@img/six-of-spades.png';
+
+import '@img/seven-of-clubs.png';
+import '@img/seven-of-diamonds.png';
+import '@img/seven-of-hearts.png';
+import '@img/seven-of-spades.png';
+
+import '@img/eight-of-clubs.png';
+import '@img/eight-of-diamonds.png';
+import '@img/eight-of-hearts.png';
+import '@img/eight-of-spades.png';
+
+import '@img/nine-of-clubs.png';
+import '@img/nine-of-diamonds.png';
+import '@img/nine-of-hearts.png';
+import '@img/nine-of-spades.png';
+
+import '@img/ten-of-clubs.png';
+import '@img/ten-of-diamonds.png';
+import '@img/ten-of-hearts.png';
+import '@img/ten-of-spades.png';
+
+import '@img/jack-of-clubs.png';
+import '@img/jack-of-diamonds.png';
+import '@img/jack-of-hearts.png';
+import '@img/jack-of-spades.png';
+
+import '@img/queen-of-clubs.png';
+import '@img/queen-of-diamonds.png';
+import '@img/queen-of-hearts.png';
+import '@img/queen-of-spades.png';
+
+import '@img/king-of-clubs.png';
+import '@img/king-of-diamonds.png';
+import '@img/king-of-hearts.png';
+import '@img/king-of-spades.png';
+
 new Blackjack(
   document.getElementById('dealer'),
   document.getElementById('deck'),
